57528dfee8143d0263094fb040e92b664a1907c1
luisin76 <luis.c.flores@gmail.com>
2024-09-04 03:25:48 -0500

    ❇️Added Nav bar menu items and icons

--- task-manager/app/Components/Sidebar/Sidebar.tsx
+++ task-manager/app/Components/Sidebar/Sidebar.tsx
@@ -2,11 +2,54 @@
 import React from "react";
 import styled from "styled-components";
 import { useGlobalState } from "../context/globalProvider";
+import Image from "next/image";
+import Link from "next/link";
+
+import menu from "../../utils/menu";
+import { link } from "fs";
+import { usePathname, useRouter } from "next/navigation";
 
 function Sidebar() {
   const { theme } = useGlobalState();
 
-  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
+
+  return (
+    <SidebarStyled theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay"></div>
+        <div className="image">
+          <Image width={70} height={70} src="/avatar1.png" alt="profile" />
+        </div>
+        <h1>
+          <span>Sin</span>
+          <span>Rostro</span>
+        </h1>
+      </div>
+      <ul className="nav-items">
+        {menu.map((item) => {
+          const link = item.link;
+          return (
+            <li
+              className={`nav- ${pathname === link ? "active" : ""}`}
+              onClick={() => {
+                handleClick(link);
+              }}
+            >
+              {item.icon}
+              <Link href={item.link}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <button></button>
+    </SidebarStyled>
+  );
 }
 
 const SidebarStyled = styled.nav`
@@ -14,6 +57,42 @@ const SidebarStyled = styled.nav`
   background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  color: ${(props) => props.theme.colorGrey3};
+
+  .profile {
+    margin: 1.5rem;
+    padding: 1ren 0.8rem;
+    position: relative;
+
+    border-radius: 1rem;
+    cursor: pointer;
+
+    font-weight: 500;
+    color: ${(props) => props.theme.colorGrey0};
+
+    display: flex;
+    align-items: center;
+  }
+
+  .profile-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(10px);
+    z-index: 0;
+    background: ${(props) => props.theme.colorBg3};
+    transition: all 0.55s linear;
+    border-radius: 1rem;
+    border: 2px solid ${(props) => props.theme.borderColor2};
+    opacity: 0.2;
+  }
 `;
 
 export default Sidebar;
--- task-manager/app/layout.tsx
+++ task-manager/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body className={inter.className}>
         <ContextProvider>
           <GlobalStyleProvider>
